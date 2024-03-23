9743c90e9d279d3d5e738fc658b3904ec429b87d
Joshua Thompson <jdtuutp45@gmail.com>
2024-03-23 15:14:17 -0400

    Update ui.py

--- game/ui.py
+++ game/ui.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 from deck import Deck
+from button import Button
+from messagebox import MessageBox
 from pile import *
 from score import *
 
@@ -27,7 +29,9 @@ class Ui:
         # Set up GUI
         self.topbar = pygame.Rect(0, 0, SCREEN_WIDTH, UI_BAR_SIZE)
         self.bottom_bar = pygame.Rect(0, SCREEN_HEIGHT-UI_BAR_SIZE, SCREEN_WIDTH, UI_BAR_SIZE)
-        self.replay_btn = pygame.Rect(5, 5, 25, 25)
+        self.klondike_btn = Button('Klondike Deal', pygame.Rect(0,0,125,Button.DEFAULT_HEIGHT+5))
+        self.vegas_btn = Button('Vegas Deal', pygame.Rect(self.klondike_btn.rect.w+1, 0, 125, Button.DEFAULT_HEIGHT+5))
+        self.win_screen = MessageBox('Do you want to play again?')
         
         # set title
         pygame.display.set_caption(SCREEN_TITLE)
@@ -64,8 +68,8 @@ class Ui:
                 elif event.type == pygame.MOUSEMOTION and self.dragged_cards:
                     self.handle_mouse_motion(pygame.mouse.get_pos())
             
-            # Fill the background with green color
-            self.screen.fill(self.bg_color)
+            # Fill the background with green color (or lighter green if game is over)
+            self.screen.fill(self.bg_color if not self.win_screen.visible else (125,218,88))
 
             # Display the deck
             self.deck.display(self.screen)
@@ -73,7 +77,10 @@ class Ui:
             # Display the GUI
             pygame.draw.rect(self.screen, UI_BAR_COLOR, self.topbar)
             pygame.draw.rect(self.screen, UI_BAR_COLOR, self.bottom_bar)
-            pygame.draw.rect(self.screen, (255,0,0), self.replay_btn)
+            self.klondike_btn.draw(self.screen)
+            self.vegas_btn.draw(self.screen)
+            if self.win_screen.visible:
+                self.win_screen.draw(self.screen)
             
             # Display the score on the top bar
             font = pygame.font.Font(None, 30)  
@@ -89,8 +96,9 @@ class Ui:
 
             # Place a win condition that restarts the game when triggered
             if len(self.deck.piles[-1].cards) == 13 and len(self.deck.piles[-2].cards) == 13 and len(self.deck.piles[-3].cards) == 13 and len(self.deck.piles[-4].cards) == 13:
-                self.setup()
-
+                self.win_screen.show()
+                self.klondike_btn.disable()
+                self.vegas_btn.disable()
 
             # Apply time penalty
             self.score.apply_time_penalty()
@@ -102,44 +110,61 @@ class Ui:
             pygame.display.flip()
 
     def handle_mouse_down(self, mouse_pos):
-        # Check to see if player clicks Replay button
-        if mouse_pos[0] > self.replay_btn.left and mouse_pos[0] < self.replay_btn.right and mouse_pos[1] > self.replay_btn.top and mouse_pos[1] < self.replay_btn.bottom:
+        # Check to see if player clicks a button
+        if self.klondike_btn.clicked(mouse_pos) and self.klondike_btn.enabled:
             self.setup()
-        
-        # Check if the click is on the deck pile
-        deck_pile = next((pile for pile in self.deck.piles if pile.pile_type == PileType.STOCK), None)
-        if deck_pile and deck_pile.is_mouse_over(mouse_pos):
-             # If the deck pile is empty
-            if not deck_pile.cards:
-                self.deck.transfer_waste_to_deck()
-                self.score.refresh_stockpile()
-            else:
-                self.deck.transfer_card_from_deck_to_waste()
-            return
-        
-        # For all other pile types
-        picked_card, picked_pile = self.deck.get_card_at_position(mouse_pos)
-        if picked_card and picked_card.discovered:
-            self.dragged_cards = []
-            self.origin_pile = picked_pile
-            self.original_positions = [] 
+        elif self.vegas_btn.clicked(mouse_pos) and self.vegas_btn.enabled:
+            print('Vegas rules applied.')
 
-            if picked_pile.pile_type == PileType.TABLEAU:
-                # Find the index of the picked card in its pile
-                index = picked_pile.cards.index(picked_card)
-                # Select the picked card and all cards above it in the pile
-                self.dragged_cards = picked_pile.cards[index:]
-                # Store original positions for each card in the dragged stack
-                self.original_positions = [(card.x, card.y) for card in self.dragged_cards]
-            else:
-                # If it's not a tableau pile, proceed with the single card
-                self.dragged_cards = [picked_card]
-                self.original_positions = [(picked_card.x, picked_card.y)]
+        # Handle win screen button clicks
+        if self.win_screen.visible:
+            if self.win_screen.clicked_yes(mouse_pos):
+                self.win_screen.hide()
+                self.klondike_btn.enable()
+                self.vegas_btn.enable()
+                self.setup()
+            elif self.win_screen.clicked_no(mouse_pos):
+                pygame.quit()
+                sys.exit()
 
-            self.offset_x = mouse_pos[0] - picked_card.x
-            self.offset_y = mouse_pos[1] - picked_card.y
+        if not self.win_screen.visible:
+            # Check if the click is on the deck pile
+            deck_pile = next((pile for pile in self.deck.piles if pile.pile_type == PileType.STOCK), None)
+            if deck_pile and deck_pile.is_mouse_over(mouse_pos):
+                 # If the deck pile is empty
+                if not deck_pile.cards:
+                    self.deck.transfer_waste_to_deck()
+                    self.score.refresh_stockpile()
+                else:
+                    self.deck.transfer_card_from_deck_to_waste()
+                return
+            
+            # For all other pile types
+            picked_card, picked_pile = self.deck.get_card_at_position(mouse_pos)
+            if picked_card and picked_card.discovered:
+                self.dragged_cards = []
+                self.origin_pile = picked_pile
+                self.original_positions = [] 
+    
+                if picked_pile.pile_type == PileType.TABLEAU:
+                    # Find the index of the picked card in its pile
+                    index = picked_pile.cards.index(picked_card)
+                    # Select the picked card and all cards above it in the pile
+                    self.dragged_cards = picked_pile.cards[index:]
+                    # Store original positions for each card in the dragged stack
+                    self.original_positions = [(card.x, card.y) for card in self.dragged_cards]
+                else:
+                    # If it's not a tableau pile, proceed with the single card
+                    self.dragged_cards = [picked_card]
+                    self.original_positions = [(picked_card.x, picked_card.y)]
+    
+                self.offset_x = mouse_pos[0] - picked_card.x
+                self.offset_y = mouse_pos[1] - picked_card.y
 
     def handle_mouse_motion(self, mouse_pos):
+        if self.win_screen.visible:
+            return
+        
         if self.dragged_cards: 
             # Calculate the new position based on the current mouse position minus the drag offset
             delta_x = mouse_pos[0] - self.offset_x
@@ -160,35 +185,36 @@ class Ui:
 
 
     def handle_mouse_up(self, mouse_pos):
-        if self.dragged_cards: 
-            target_pile = self.deck.get_pile_at_position(mouse_pos)
-            move_valid = False
-
-            if target_pile:
-                # Get the bottom card for validation
-                bottom_card = self.dragged_cards[0]
-
-                # Check if moving the bottom card to the target pile is valid
-                move_valid = self.deck.is_valid_move(bottom_card, self.origin_pile, target_pile)
-
-                if move_valid:
-                    # Move each card in the dragged stack to the target pile if the bottom card's move is valid
-                    for card in self.dragged_cards:
-                        self.snap_card_to_pile(card, target_pile)
-                        self.deck.move_card(card, self.origin_pile, target_pile, self.score)
-
-            else:
+        if not self.win_screen.visible:
+            if self.dragged_cards: 
+                target_pile = self.deck.get_pile_at_position(mouse_pos)
                 move_valid = False
-
-            if not move_valid:
-                # If the move is invalid, or no target pile is identified, revert cards to their original positions
-                for card, (original_x, original_y) in zip(self.dragged_cards, self.original_positions):
-                    card.set_position(original_x, original_y)
-                    if card not in self.origin_pile.cards:
-                        self.origin_pile.cards.append(card)
-
-            # Clear the list of dragged cards after dropping
-            self.dragged_cards = []
+    
+                if target_pile:
+                    # Get the bottom card for validation
+                    bottom_card = self.dragged_cards[0]
+    
+                    # Check if moving the bottom card to the target pile is valid
+                    move_valid = self.deck.is_valid_move(bottom_card, self.origin_pile, target_pile)
+    
+                    if move_valid:
+                        # Move each card in the dragged stack to the target pile if the bottom card's move is valid
+                        for card in self.dragged_cards:
+                            self.snap_card_to_pile(card, target_pile)
+                            self.deck.move_card(card, self.origin_pile, target_pile, self.score)
+    
+                else:
+                    move_valid = False
+    
+                if not move_valid:
+                    # If the move is invalid, or no target pile is identified, revert cards to their original positions
+                    for card, (original_x, original_y) in zip(self.dragged_cards, self.original_positions):
+                        card.set_position(original_x, original_y)
+                        if card not in self.origin_pile.cards:
+                            self.origin_pile.cards.append(card)
+    
+                # Clear the list of dragged cards after dropping
+                self.dragged_cards = []
 
     def snap_card_to_pile(self, card, target_pile):
         # Snapping back to the original pile
